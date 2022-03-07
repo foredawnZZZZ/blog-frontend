f47f80856bf8a8e94e31298f8f5228ce9810d365
foredawnZZZZ <18602151918@163.com>
2022-03-07 14:18:26 +0800

    add vercel.json

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
   <header
    class="masthead"
    :style="{
-    backgroundImage: `url(http://106.75.226.89:1337${general.cover.url})`
+    backgroundImage: `url(${general.cover.url})`
    }"
   >
    <div class="container position-relative px-4 px-lg-5">
